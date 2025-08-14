7588d5abaaa29d88b6e8deae7480011ac237b06e
islamashraf2003 <islamashraf520@gmail.com>
2025-08-14 18:28:29 +0300

    refactor: :recycle: added engzly app , remove unse code

---
+++ lib/engzly_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EngzlyApp extends StatelessWidget {
+  const EngzlyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
