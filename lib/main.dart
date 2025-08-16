ae152f3993b1e419c23905e8770bb52ae1182474
RaWan Magdy <rawan.magdy.fahmy@gmail.com>
2025-08-16 18:36:31 +0300

    change ndkVersion to "27.0.12077973"

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/routing/app_router.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -32,6 +34,8 @@ class _MyAppState extends State<MyApp> {
           debugShowCheckedModeBanner: false,
           theme: AppTheme.appTheme,
           initialRoute: RouteName.login,
+          onGenerateRoute: AppRoutes.onGenerateRoute,
+
         );
       },
     );
