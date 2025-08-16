00e28a5f1170210d75be0611076b1cac26a9cb91
islamashraf2003 <islamashraf520@gmail.com>
2025-08-16 21:20:48 +0300

    feat: :art: added ScreenUtilInit in EngzlyApp

--- lib/engzly_app.dart
+++ lib/engzly_app.dart
@@ -1,11 +1,24 @@
 import 'package:engzly/features/home/ui/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EngzlyApp extends StatelessWidget {
   const EngzlyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Engzly',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+          home: const HomeScreen(),
+        );
+      },
+    );
   }
 }
